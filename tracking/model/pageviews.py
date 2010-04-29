dc3c9959b0e12d0e93c91a46b767eb768b1e19e2
Chris Reid <chris.reid@suite101.com>
2010-04-29 15:55:01 -0700

    updates postgres connection string in development.ini.bak file

---
+++ tracking/model/pageviews.py
@@ -0,0 +1,17 @@
+import sqlalchemy as sa
+
+# import base from meta class
+from tracking.model.meta import Base
+
+# Using SQLAlchemy 0.5 optional declarative syntax
+# More info here: http://pylonshq.com/docs/en/0.9.7/models/
+# and here: http://pylonshq.com/pasties/1000
+
+class Pageviews(Base):
+    __tablename__ = "pageviews"
+
+    st_id = sa.Column(sa.types.Integer, sa.Sequence('page_seq_id', optional=True), primary_key=True)
+    st_user_agent = sa.Column(sa.types.String(255), default='')
+    st_url = sa.Column(sa.types.String(8000), default='')
+    st_spider_date = sa.Column(sa.types.DateTime, default='')
+
