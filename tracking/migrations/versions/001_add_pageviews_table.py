2eefc5ed71e596d7f6d7a6cb83038a0b731b9811
Chris Reid <chris.reid@suite101.com>
2010-05-03 13:59:52 -0700

    drops st_ from column names

--- tracking/migrations/versions/001_add_pageviews_table.py
+++ tracking/migrations/versions/001_add_pageviews_table.py
@@ -1,7 +1,6 @@
 from sqlalchemy import *
 from migrate import *
 
-from sqlalchemy.ext.declarative import declarative_base
 
 meta = MetaData(migrate_engine)
 pageviews = Table('pageviews', meta,
--- tracking/model/pageviews.py
+++ tracking/model/pageviews.py
@@ -10,8 +10,8 @@ from sqlalchemy.ext.declarative import declarative_base
 class Pageviews(Base):
     __tablename__ = "pageviews"
 
-    st_id = sa.Column(sa.types.Integer, sa.Sequence('page_seq_id', optional=True), primary_key=True)
-    st_user_agent = sa.Column(sa.types.String(255), default='')
-    st_url = sa.Column(sa.types.String(8000), default='')
-    st_spider_date = sa.Column(sa.types.DateTime, default='')
+    id = sa.Column(sa.types.Integer, sa.Sequence('page_seq_id', optional=True), primary_key=True)
+    user_agent = sa.Column(sa.types.String(255), default='')
+    url = sa.Column(sa.types.String(8000), default='')
+    spider_date = sa.Column(sa.types.DateTime, default='')
 
