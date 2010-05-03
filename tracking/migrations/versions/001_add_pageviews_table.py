f503862844477a32c8d9574961ed1b1255b5e5e1
Chris Reid <chris.reid@suite101.com>
2010-05-03 13:42:56 -0700

    adds 001 migration

---
+++ tracking/migrations/versions/001_add_pageviews_table.py
@@ -0,0 +1,21 @@
+from sqlalchemy import *
+from migrate import *
+
+from sqlalchemy.ext.declarative import declarative_base
+
+meta = MetaData(migrate_engine)
+pageviews = Table('pageviews', meta,
+                Column('st_id', Integer, Sequence('page_seq_id', optional=True), primary_key=True),
+                Column('st_user_agent', String(255), default=''),
+                Column('st_url', String(8000), default=''),
+                Column('st_spider_date', DateTime, default=''),
+                )
+
+def upgrade():
+    # Upgrade operations go here. Don't create your own engine; use the engine
+    # named 'migrate_engine' imported from migrate.
+    pageviews.create()
+    
+def downgrade():
+    # Operations to reverse the above upgrade go here.
+    pageviews.drop()
