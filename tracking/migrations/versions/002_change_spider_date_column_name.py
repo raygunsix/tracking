8d61dce1e2a19ffccbd83caad9b0224d1b8463a2
Chris Reid <chris.reid@suite101.com>
2010-05-03 15:47:56 -0700

    adds migration to alter spider_date column name

--- tracking/migrations/versions/002_change_spider_date_column_name.py
+++ tracking/migrations/versions/002_change_spider_date_column_name.py
@@ -1,15 +1,29 @@
 from sqlalchemy import *
 from migrate import *
+import migrate.changeset
 
 meta = MetaData(migrate_engine)
 
-col = Column('st_spider_date')
+pageviews_new = Table('pageviews', meta,
+                Column('st_id', Integer, Sequence('page_seq_id', optional=True), primary_key=True),
+                Column('st_user_agent', String(255), default=''),
+                Column('st_url', String(8000), default=''),
+                Column('spider_date', DateTime, default=''),
+                )
+
+pageviews_old = Table('pageviews', meta,
+                Column('st_id', Integer, Sequence('page_seq_id', optional=True), primary_key=True),
+                Column('st_user_agent', String(255), default=''),
+                Column('st_url', String(8000), default=''),
+                Column('st_spider_date', DateTime, default=''),
+                useexisting=True
+                )
 
 def upgrade():
-    # Upgrade operations go here. Don't create your own engine; use the engine
-    # named 'migrate_engine' imported from migrate.
-    col.alter(name='spider_date')
+    # Upgrade operations go here.
+    pageviews_old.c.st_spider_date.alter(name='spider_date')
 
 def downgrade():
     # Operations to reverse the above upgrade go here.
-    col.alter(name='st_spider_date')
+    #col.alter(name='st_spider_date')
+    pageviews_new.c.spider_date.alter(name='st_spider_date')
