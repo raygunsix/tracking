6d4f63be2c6042225ac8f57290b89e0b2935a3df
Chris Reid <chris.reid@suite101.com>
2010-05-10 17:33:01 -0700

    adds migration scripts

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
 
-main(url='postgres://postgres:suite101@172.16.31.106:5432/collector',repository='migrations')
+main(url='postgres://postgres:suite101@172.16.31.106:5432/tracking',repository='migrations')
